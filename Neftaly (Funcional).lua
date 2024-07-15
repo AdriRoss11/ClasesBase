31f1ecb9540c88d88357b35ffbc9b09bb378ba79
AdrianRossel <arossel.ngc@gmail.com>
2024-07-15 13:32:05 -0400

    primerpase

---
+++ Neftaly (Funcional).lua
@@ -0,0 +1,31 @@
+Neftaly (Funcional)
+-- Renovación (Cliente a reutilziar)
+Tiempo de demora de la evaluacion
+Ingresos Ventas
+Reconocimiento Paddi vs Froddi
+Resolucion de Comite (cargado)
+Traje a la medida
+
+EvaGarcia ()
+Experiencia en Robotización de Procesos
+Certificación en Robotización
+Generación de Reporteria
+Expediente Digital Conformado
+Implementación Realizada similares a FDL
+
+DarwinSolano (Funcional)
+Registro de inventario Pecuario (Ganadero)
+
+LuisCuadra (TI)
+Sesión de temas tecnicos
+
+ReynaldoSanchez()
+Consumo de Buros Offline
+Memoria de Calculo
+
+ECastillo ()
+Tiempos de Soporte, para el piloto
+
+ErnestoVasquez ()
+
+Estadisticas
---
+++ Untitled-1.md
@@ -0,0 +1,13 @@
+* Navegador Edge
+* Servidor BD no requiere internet
+* Trabaja con una DMZ
+* CORE JACKSON
+* No clonan el servidor
+* Comunicación Limpia *tokken*
+* Diccionario de Datos
+* Medir los leds BD
+* Actualización de Sistema Operativo de Windows
+* INFOCENTER RECUPERAR DE IDEPRO
+* Precautelar las consultas directas a la BD
+* Miercoles (10 am) ambos 2:30
+* 
